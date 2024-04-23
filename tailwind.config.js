f31f57425d4cdc927fbb68c9f9925ae1a0965b36
yash raj <yashraj0207@outlook.com>
2024-04-23 11:42:50 +0530

    change sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		fontFamily: {
+			sans: ['Bricolage Grotesque', 'Helvetica', 'Arial', 'sans-serif']
+		},
+		extend: {}
+	},
+	plugins: []
+};
