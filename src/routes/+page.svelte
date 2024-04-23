76ff7bb0dd356f932a4d9092943058496567f3fd
yash raj <yashraj0207@outlook.com>
2024-04-23 10:23:14 +0530

    initial commit with tailwind

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div class="p-6">
+	<h1 class="font-bold text-3xl pb-2">Welcome to SvelteKit</h1>
+	<p>
+		Visit <a
+			href="https://kit.svelte.dev"
+			class="text-blue-600 hover:text-blue-700 underline underline-offset-2 hover:no-underline transition-all"
+			>kit.svelte.dev</a
+		> to read the documentation
+	</p>
+</div>
