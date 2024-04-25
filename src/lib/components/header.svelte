aad0ed78c401fb21255af74c9fdcd131c7b2ad62
yash raj <yashraj0207@outlook.com>
2024-04-25 16:41:22 +0530

    create header for desktop

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,25 +1,49 @@
 <script lang="ts">
 	import { cx } from 'class-variance-authority';
 	import { textStyles } from './text/text-styles';
+	import HalfLoader from '$lib/icons/half-loader.icon.svelte';
 
-	const LINKS: Array<{ label: string; route: string }> = [
+	type TLink = {
+		id: string;
+		label: string;
+		route: string;
+	};
+
+	const LINKS: Array<TLink> = [
 		{
+			id: 'blog',
 			label: 'Blog',
 			route: '/blog'
 		},
 		{
+			id: 'resume',
 			label: 'Resume',
 			route: '/resume'
+		},
+		{
+			id: 'get-in-touch',
+			label: 'Get in touch',
+			route: '/#get-in-touch'
 		}
 	];
 </script>
 
 <header class="w-full">
-	<nav class="">
-		<ul class="flex items-center gap-6">
+	<nav class="flex items-center">
+		<div class="p-2 -m-2">
+			<HalfLoader class="w-8 h-8" />
+		</div>
+
+		<ul class="flex items-center gap-6 ml-auto">
 			{#each LINKS as link}
 				<li>
-					<a href={link.route} class={cx(textStyles({ variant: 'b2-light' }))}>{link.label}</a>
+					<a
+						href={link.route}
+						class={cx(
+							textStyles({ variant: link.id === 'get-in-touch' ? 'b2' : 'b2-light' }),
+							link.id === 'get-in-touch' && 'px-4 py-2 -mx-2 bg-blue-100 rounded-lg text-blue-700'
+						)}>{link.label}</a
+					>
 				</li>
 			{/each}
 		</ul>
---
+++ src/lib/icons/half-loader.icon.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { cx } from 'class-variance-authority';
+
+	interface $$Props {
+		class?: string;
+	}
+</script>
+
+<svg viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg" class={cx($$props.class)}>
+	<path
+		d="M16 3.1665V8.49984"
+		stroke="#1C1917"
+		stroke-width="3"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M6.5733 7.07324L10.3466 10.8466"
+		stroke="#1C1917"
+		stroke-width="3"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M2.66669 16.5H8.00002"
+		stroke="#1C1917"
+		stroke-width="3"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M24 16.5H29.3333"
+		stroke="#1C1917"
+		stroke-width="3"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M21.6533 10.8466L25.4267 7.07324"
+		stroke="#1C1917"
+		stroke-width="3"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>
